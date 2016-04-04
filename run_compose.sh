dfd67a96bb88b610dfe2a614f9ac2f36ec1188c7
Dav Clark <davclark@berkeley.edu>
2016-04-04 12:52:18 -0700

    HOSTIP was not getting set!

--- run_compose.sh
+++ run_compose.sh
@@ -1,5 +1,5 @@
 docker-machine start sz-tokyo
 docker-machine regenerate-certs sz-tokyo
 eval $(docker-machine env sz-tokyo)
-HOSTIP=$(docker-machine ip sz-tokyo) docker-compose up -d
-docker-compose logs
+HOSTIP=$(docker-machine ip sz-tokyo); docker-compose up -d
+exec docker-compose logs
